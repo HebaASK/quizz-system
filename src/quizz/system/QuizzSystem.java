765e92a12f9b2c30c8818dcfc58d522a1106a8f2
HebaASK <heebbaa8@gmail.com>
2024-11-05 15:00:54 +0300

    First commit

---
+++ src/quizz/system/QuizzSystem.java
@@ -0,0 +1,20 @@
+
+
+
+package quizz.system;
+
+
+/**
+ *
+ * @author DELL
+ */
+public class QuizzSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
