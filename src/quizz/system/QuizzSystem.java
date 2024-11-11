4fbface5252993412f5396dfe78c2b5f362ed0e8
HebaASK <heebbaa8@gmail.com>
2024-11-11 13:09:19 +0300

    Update QuizzSystem.java

--- src/quizz/system/QuizzSystem.java
+++ src/quizz/system/QuizzSystem.java
@@ -4,17 +4,10 @@
 package quizz.system;
 
 
-/**
- *
- * @author DELL
- */
 public class QuizzSystem {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
