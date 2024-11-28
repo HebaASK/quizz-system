ef6638fb2669f8351810e26d0df897d619a20957
HebaASK <heebbaa8@gmail.com>
2024-11-28 12:40:12 +0300

    Update login.java

--- src/quizz/system/login.java
+++ src/quizz/system/login.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package quizz.system;
 
 import java.util.Scanner;
@@ -10,7 +7,7 @@ class Login {
         public void loginUser() {
             Scanner scanner = new Scanner(System.in);
 
-            // Collect username and password
+            
             System.out.println("\n--- Login Page ---");
             System.out.print("Enter Username: ");
             String username = scanner.nextLine();
@@ -25,7 +22,7 @@ class Login {
             System.out.print("Enter your choice (1 or 2): ");
             int roleChoice = scanner.nextInt();
 
-            // Display login success message based on role
+            
             if (roleChoice == 1) {
                 System.out.println("\nWelcome, Teacher " + username + "!");
             } else if (roleChoice == 2) {
